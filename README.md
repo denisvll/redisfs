f8baf183bb2fc8b07765ce77b4c56c9130fe9ddd
denisvll <denis.puchkin@gmail.com>
2019-02-17 22:14:22 +0300

    Dev (#4)
    add ci jobs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # redisfs
+Master
+
+[![Build Status](https://travis-ci.org/denisvll/redisfs.svg?branch=master)](https://travis-ci.org/denisvll/redisfs)
+
+
+Dev
+
+[![Build Status](https://travis-ci.org/denisvll/redisfs.svg?branch=dev)](https://travis-ci.org/denisvll/redisfs)
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
-import io
 import sys
 import redis
 import json
 import errno
-from stat import S_IFDIR, S_IFLNK, S_IFREG
+from stat import S_IFDIR
 from time import time
 
-
 from fuse import FUSE, FuseOSError, Operations
+
 
 class Passthrough(Operations):
     def __init__(self, redis_client):
@@ -30,7 +29,7 @@ class Passthrough(Operations):
         print("getattr" + path)
         print("key = " + path)
         try:
-            attr = json.loads(self.redis_client.hget(path,"attr"))
+            attr = json.loads(self.redis_client.hget(path, "attr"))
         except TypeError:
             print("not found")
             raise FuseOSError(errno.ENOENT)
@@ -72,30 +71,31 @@ class Passthrough(Operations):
         print("mkdir")
         parent = os.path.dirname(path)
         children = os.path.basename(path)
-        now=time()
+        now = time()
         attr = dict(
-            st_atime = now,
-            st_ctime = now,
-            st_gid = 1000,
-            st_mode = (S_IFDIR | mode),
-            st_mtime = now,
-            st_nlink = 2,
-            st_size = 0,
-            st_uid = 1000
+            st_atime=now,
+            st_ctime=now,
+            st_gid=1000,
+            st_mode=(S_IFDIR | mode),
+            st_mtime=now,
+            st_nlink=2,
+            st_size=0,
+            st_uid=1000
         )
         print(attr)
-        self.redis_client.hset(path,"type","dir")
-        self.redis_client.hset(path,"attr",json.dumps(attr))
-        self.redis_client.hset(path,"parent", path)
-        self.redis_client.lpush(parent + ":children",children)
+        self.redis_client.hset(path, "type", "dir")
+        self.redis_client.hset(path, "attr", json.dumps(attr))
+        self.redis_client.hset(path, "parent", path)
+        self.redis_client.lpush(parent + ":children", children)
         return 0
 
     def statfs(self, path):
         print("statfs")
         stv = os.statvfs(path)
         return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
-            'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
-            'f_frsize', 'f_namemax'))
+                                                         'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files',
+                                                         'f_flag',
+                                                         'f_frsize', 'f_namemax'))
 
     def unlink(self, path):
         parent = os.path.dirname(path)
@@ -103,7 +103,7 @@ class Passthrough(Operations):
         all_keys = list(self.redis_client.hgetall(path).keys())
         print(all_keys)
         self.redis_client.hdel(path, *all_keys)
-        self.redis_client.lrem(parent + ":children",0,children)
+        self.redis_client.lrem(parent + ":children", 0, children)
         print("unlink")
         return 0
 
@@ -123,34 +123,32 @@ class Passthrough(Operations):
         print("utimems")
         return 0
 
-
-
     def open(self, path, flags):
         print("open")
         return 0
 
     def create(self, path, mode, fi=None):
         print("create")
-        print (path)
+        print(path)
         parent = os.path.dirname(path)
         children = os.path.basename(path)
         print(mode)
         now = time()
         attr = dict(
-            st_atime = now,
-            st_ctime = now,
-            st_gid = 1000,
-            st_mode = 33188,
-            st_mtime = now,
-            st_nlink = 1,
-            st_size = 0,
-            st_uid = 1000
+            st_atime=now,
+            st_ctime=now,
+            st_gid=1000,
+            st_mode=33188,
+            st_mtime=now,
+            st_nlink=1,
+            st_size=0,
+            st_uid=1000
         )
 
-        self.redis_client.hset(path,"type","file")
-        self.redis_client.hset(path,"attr",json.dumps(attr))
-        self.redis_client.hset(path,"parent", path )
-        self.redis_client.lpush(parent + ":children",children)
+        self.redis_client.hset(path, "type", "file")
+        self.redis_client.hset(path, "attr", json.dumps(attr))
+        self.redis_client.hset(path, "parent", path)
+        self.redis_client.lpush(parent + ":children", children)
         return 0
 
     def read(self, path, length, offset, fh):
@@ -158,36 +156,33 @@ class Passthrough(Operations):
         print("Length", length)
         print("Offset", offset)
 
-        payload = self.redis_client.hget(path,"payload")
+        payload = self.redis_client.hget(path, "payload")
         reqested_payload = payload[offset:(offset + length)]
         print("got from redis:", len(payload))
-        print ("Got:", len(reqested_payload))
+        print("Got:", len(reqested_payload))
         return reqested_payload
 
     def write(self, path, buf, offset, fh):
         print("write")
         now = time()
-        attr = json.loads(self.redis_client.hget(path,"attr"))
-
-
+        attr = json.loads(self.redis_client.hget(path, "attr"))
 
         attr['st_atime'] = now
         attr['st_mtime'] = now
 
         if offset > 0:
-            payload = self.redis_client.hget(path,"payload") + buf
+            payload = self.redis_client.hget(path, "payload") + buf
             print("write next chunk", len(buf))
         else:
             payload = buf
-            print("got first chunk",len(buf))
+            print("got first chunk", len(buf))
 
-        self.redis_client.hset(path,"payload",payload)
+        self.redis_client.hset(path, "payload", payload)
 
         attr['st_size'] = len(payload)
 
-        self.redis_client.hset(path,"attr",json.dumps(attr))
+        self.redis_client.hset(path, "attr", json.dumps(attr))
         return len(buf)
-
 
     def truncate(self, path, length, fh=None):
         print("truncate")
@@ -209,7 +204,27 @@ class Passthrough(Operations):
 
 def main(mountpoint):
     redis_client = redis.Redis(host='localhost', port=6379, db=0)
-    FUSE(Passthrough(redis_client), mountpoint, nothreads=True, foreground=True)
+
+    # check our fs has root, if not - create it!
+    if not redis_client.hget("/", "attr"):
+        print("creating /")
+        now = time()
+        attr = dict(
+            st_atime=now,
+            st_ctime=now,
+            st_gid=0,
+            st_mode=16895,
+            st_mtime=now,
+            st_nlink=2,
+            st_size=0,
+            st_uid=0
+        )
+        redis_client.hset("/", "type", "dir")
+        redis_client.hset("/", "attr", json.dumps(attr))
+        redis_client.hset("/", "parent", "none")
+
+    FUSE(Passthrough(redis_client), mountpoint, nothreads=True, foreground=False, **{'allow_other': True})
+
 
 if __name__ == '__main__':
     main(sys.argv[1])
